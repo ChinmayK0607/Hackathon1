1b2931aabb18b018977f78b74ccc2ae63b081d3e
ChinmayK0607 <114411195+ChinmayK0607@users.noreply.github.com>
2023-10-28 18:35:46 +0530

    Update models.py

--- Backend/MediaUpload/models.py
+++ Backend/MediaUpload/models.py
@@ -1,8 +1,22 @@
 from db import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    # Define one-to-many relationships with photos and videos
+   # photos = db.relationship('Img', backref='user', lazy=True)
+   # videos = db.relationship('Video', backref='user', lazy=True)
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    video = db.Column(db.LargeBinary, nullable=False)
     name = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
