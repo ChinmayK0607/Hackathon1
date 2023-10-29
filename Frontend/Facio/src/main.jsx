8dc79e403a89b8ad6675487e0dd20cdf3fe0f3a5
Om Sarraf <itsomsarraf+github@gmail.com>
2023-10-29 21:55:28 +0530

    routing left, uncomment in app.jsx to see pages

--- Frontend/Facio/src/main.jsx
+++ Frontend/Facio/src/main.jsx
@@ -1,12 +1,10 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <Router>
-      <App />
-    </Router>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
